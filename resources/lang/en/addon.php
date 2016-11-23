c440b0f43c96bd6f32f6dc48ebb80ad6312577dd
Matthew Davies <daviesgeek@icloud.com>
2016-11-23 14:15:03 -0800

    Initial commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Emoji',
+    'name'        => 'Emoji Plugin',
+    'description' => 'An emoji plugin for Twig'
+];
---
+++ src/EmojiPluginServiceProvider.php
@@ -0,0 +1,38 @@
+<?php namespace Daviesgeek\EmojiPlugin;
+
+use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
+
+class EmojiPluginServiceProvider extends AddonServiceProvider
+{
+
+    protected $plugins = [];
+
+    protected $commands = [];
+
+    protected $routes = [];
+
+    protected $middleware = [];
+
+    protected $listeners = [];
+
+    protected $aliases = [];
+
+    protected $bindings = [];
+
+    protected $providers = [];
+
+    protected $singletons = [];
+
+    protected $overrides = [];
+
+    protected $mobile = [];
+
+    public function register()
+    {
+    }
+
+    public function map()
+    {
+    }
+
+}
