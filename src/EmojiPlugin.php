678e3e2080159c924a35c2fcdbc79b91b81a609d
Ryan Thompson <ryan@pyrocms.com>
2016-12-18 19:11:21 -0600

    Remove unsed

--- src/EmojiPlugin.php
+++ src/EmojiPlugin.php
@@ -1,64 +1,11 @@
 <?php namespace Daviesgeek\EmojiPlugin;
 
 use Anomaly\Streams\Platform\Addon\Plugin\Plugin;
-use Twig_NodeVisitorInterface;
-use Twig_SimpleFunction;
 use Emojione\Emojione;
-use Twig_Environment;
+use Twig_SimpleFunction;
 
 class EmojiPlugin extends Plugin
 {
-
-    /**
-     * Initializes the runtime environment.
-     *
-     * This is where you can load some file that contains filter functions for instance.
-     *
-     * @param Twig_Environment $environment The current Twig_Environment instance
-     */
-    public function initRuntime(Twig_Environment $environment)
-    {
-    }
-
-    /**
-     * Returns the token parser instances to add to the existing list.
-     *
-     * @return array An array of Twig_TokenParserInterface or Twig_TokenParserBrokerInterface instances
-     */
-    public function getTokenParsers()
-    {
-        return [];
-    }
-
-    /**
-     * Returns the node visitor instances to add to the existing list.
-     *
-     * @return Twig_NodeVisitorInterface[] An array of Twig_NodeVisitorInterface instances
-     */
-    public function getNodeVisitors()
-    {
-        return [];
-    }
-
-    /**
-     * Returns a list of filters to add to the existing list.
-     *
-     * @return array An array of filters
-     */
-    public function getFilters()
-    {
-        return [];
-    }
-
-    /**
-     * Returns a list of tests to add to the existing list.
-     *
-     * @return array An array of tests
-     */
-    public function getTests()
-    {
-        return [];
-    }
 
     /**
      * Returns a list of functions to add to the existing list.
@@ -68,30 +15,11 @@ class EmojiPlugin extends Plugin
     public function getFunctions()
     {
         return [
-            new Twig_SimpleFunction('emoji', function($string) {
+            new Twig_SimpleFunction(
+                'emoji', function ($string) {
                 return Emojione::shortnameToImage(':' . str_replace(':', '', $string) . ':');
-            }),
+            }
+            ),
         ];
     }
-
-    /**
-     * Returns a list of operators to add to the existing list.
-     *
-     * @return array An array of operators
-     */
-    public function getOperators()
-    {
-        return [];
-    }
-
-    /**
-     * Returns a list of global variables to add to the existing list.
-     *
-     * @return array An array of global variables
-     */
-    public function getGlobals()
-    {
-        return [];
-    }
-
 }
